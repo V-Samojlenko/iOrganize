48fe69659abed64274bafb88f5ab057c585f8cfa
v.samojlenko <vladislav.samojlenko@icloud.com>
2019-07-22 19:53:35 +0200

    Base angular

--- iOrganize-frontend/src/main/java/com/vs/organize/controller/MainController.java
+++ iOrganize-frontend/src/main/java/com/vs/organize/controller/MainController.java
@@ -8,9 +8,5 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 
-  @RequestMapping({"/", "/index"})
-  public String index(Model model) {
-    return "index";
-  }
 
 }
