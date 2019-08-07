6cbc490c799a196786cecfd937f39e5eeb8f2d1a
vsamojlenko <vladislav.samojlenko@icloud.com>
2019-08-07 17:32:06 +0200

    Group title update

--- iOrganize-angular/src/app/group/group.component.ts
+++ iOrganize-angular/src/app/group/group.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit,Renderer2, ViewChild, ElementRef} from '@angular/core';
 import {Group} from "../model/group";
 import {Board} from "../model/board";
 import {RestService} from "../rest.service";
@@ -10,13 +10,14 @@ import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag
   styleUrls: ['./group.component.css']
 })
 export class GroupComponent implements OnInit {
-
+  editTitle: boolean = false;
   @Input()
   group: Group;
   @Input()
   board: Board;
+  @ViewChild('titleInput',{static:false}) groupTitleInput:ElementRef;
 
-  constructor(public restService: RestService) {
+  constructor(private restService: RestService) {
   }
 
   ngOnInit(): void {
@@ -25,17 +26,35 @@ export class GroupComponent implements OnInit {
 
   drop(event: CdkDragDrop<Group>) {
     if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data.nodes, event.previousIndex, event.currentIndex);
-      this.restService.updateGroup(event.previousContainer.data).subscribe();
+      moveItemInArray(this.group.nodes, event.previousIndex, event.currentIndex);
+      this.restService.updateGroup(this.group).subscribe();
     } else {
       transferArrayItem(event.previousContainer.data.nodes,
-          event.container.data.nodes,
+          this.group.nodes,
           event.previousIndex,
           event.currentIndex);
       this.restService.updateGroup(event.previousContainer.data).subscribe(data => {
-        this.restService.updateGroup(event.container.data).subscribe();
+        this.restService.updateGroup(this.group).subscribe();
       });
     }
   }
 
+  showInput() {
+    this.editTitle = true;
+    setTimeout(() => {
+      this.groupTitleInput.nativeElement.focus();
+    }, 0);
+
+  }
+
+  onKeydown(event) {
+    if (event.key === "Enter") {
+     this.updateGroupTitle()
+    }
+  }
+
+  updateGroupTitle(){
+    this.editTitle = false;
+    this.restService.updateGroup(this.group).subscribe();
+  }
 }
